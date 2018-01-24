675d7b104414ad8425a0be5eb17b9c1549c1a4ee
Mark Blanford <markytorture@hotmail.com>
2018-01-24 13:36:49 +0000

    basic one-page app working

--- app/src/main/java/example/codeclan/com/rockpaperscissors/RockPaperScissorsActivity.java
+++ app/src/main/java/example/codeclan/com/rockpaperscissors/RockPaperScissorsActivity.java
@@ -2,12 +2,38 @@ package example.codeclan.com.rockpaperscissors;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RockPaperScissorsActivity extends AppCompatActivity {
+
+    RockPaperScissors game;
+    TextView game_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rock_paper_scissors);
+    }
+
+    public void onRockButtonClick(View button){
+        game = new RockPaperScissors("rock");
+        String result = game.play();
+        game_result = findViewById(R.id.game_result);
+        game_result.setText(result);
+    }
+
+    public void onPaperButtonClick(View button) {
+        game = new RockPaperScissors("paper");
+        String result = game.play();
+        game_result = findViewById(R.id.game_result);
+        game_result.setText(result);
+    }
+
+    public void onScissorsButtonClick(View button) {
+        game = new RockPaperScissors("scissors");
+        String result = game.play();
+        game_result = findViewById(R.id.game_result);
+        game_result.setText(result);
     }
 }
