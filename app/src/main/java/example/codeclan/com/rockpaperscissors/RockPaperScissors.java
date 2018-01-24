de38def73721eb55e149e4a6e5ac2b4429899ad1
Mark Blanford <markytorture@hotmail.com>
2018-01-24 11:37:10 +0000

    initial commit - game logic written

---
+++ app/src/main/java/example/codeclan/com/rockpaperscissors/RockPaperScissors.java
@@ -0,0 +1,47 @@
+package example.codeclan.com.rockpaperscissors;
+
+import java.util.Random;
+
+/**
+ * Created by user on 24/01/2018.
+ */
+
+public class RockPaperScissors {
+
+    private String playerHand;
+    private String computerHand;
+
+    public RockPaperScissors(String playerHand){
+        this.playerHand = playerHand;
+    }
+
+    public String play(){
+
+        this.computerHand = computerHand();
+
+        if(playerHand == "rock" && computerHand == "paper"){
+            return "Paper beats rock, computer wins!";
+        } else if(playerHand == "rock" && computerHand == "scissors"){
+            return "Rock beats scissors, player 1 wins!";
+        } else if(playerHand == "paper" && computerHand == "scissors"){
+            return "Scissors beats paper, computer wins!";
+        } else if(playerHand == "paper" && computerHand == "rock"){
+            return "Paper beats rock, player 1 wins!";
+        } else if(playerHand == "scissors" && computerHand == "rock"){
+            return "Rock beats scissors, computer wins!";
+        } else if(playerHand == "scissors" && computerHand == "paper"){
+            return "Scissors beats paper, player 1 wins!";
+        } else return"It's a draw!";
+    }
+
+    public String computerHand(){
+        Random rand = new Random();
+        int  n = rand.nextInt(3) + 1;
+        if(n == 1){
+            return "rock";
+        } else if(n == 2){
+            return "paper";
+        } else
+            return "scissors";
+    }
+}
---
+++ app/src/test/java/example/codeclan/com/rockpaperscissors/RockPaperScissorsTest.java
@@ -0,0 +1,39 @@
+package example.codeclan.com.rockpaperscissors;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by user on 24/01/2018.
+ */
+
+public class RockPaperScissorsTest {
+
+    RockPaperScissors rockPaperScissors;
+    RockPaperScissors rockPaperScissors2;
+    RockPaperScissors rockPaperScissors3;
+
+    @Before
+    public void before(){
+        rockPaperScissors = new RockPaperScissors("rock");
+        rockPaperScissors2 = new RockPaperScissors("rock");
+        rockPaperScissors3 = new RockPaperScissors("scissors");
+    }
+
+    @Test
+    public void paperBeatsRock(){
+        assertEquals("Paper beats rock, computer wins!", rockPaperScissors.play());
+    }
+
+    @Test
+    public void scissorsBeatsPaper(){
+        assertEquals("Scissors beats paper, player 1 wins!", rockPaperScissors3.play());
+    }
+
+    @Test
+    public void canDraw(){
+        assertEquals("It's a draw!", rockPaperScissors2.play());
+    }
+}
